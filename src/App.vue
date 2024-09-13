7dabedeb126defbba97bca80938fadd58f04e51d
sr <sr@kali.kali>
2024-09-13 11:28:11 +0600

    Vue js project setup

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+  import { ref } from 'vue'
+  const count = ref(0)
+  console.log(count);
+  
+</script>
+
+<template>
+  <button @click="count++">Count is: {{ count }}</button>
+</template>
+
+<style scoped>
+button {
+  font-weight: bold;
+  background-color: red;
+  padding: 10px 20px;
+  border: none;
+  display: inline-block;
+  border-radius: 5px;
+
+}
+</style>
+
+
